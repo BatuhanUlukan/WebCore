a60086a97cf98854bbadf077ab2d3dda1e3ab0f9
Batuhan Ulukan <batuhanulukan_55@protonmail.com>
2023-09-01 00:03:23 +0300

    Update

---
+++ WebCore/ViewComponents/JSViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebCore.ViewComponents
+{
+
+	public class JSViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			// ViewComponent'in görselleştirileceği görünümü döndürün
+			return View();
+		}
+	}
+
+}
---
+++ WebCore/ViewComponents/SideBarViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebCore.ViewComponents
+{
+
+	public class SideBarViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			// ViewComponent'in görselleştirileceği görünümü döndürün
+			return View();
+		}
+	}
+
+}
